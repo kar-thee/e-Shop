8e0b131c6734899abcd79a3705cfb1b75dca2253
Kartheekeyan S <fsdkartheek@gmail.com>
2024-10-03 17:45:37 +0530

    Added more data for better ui/ux

--- src/hooks/defaultState.jsx
+++ src/hooks/defaultState.jsx
@@ -11,7 +11,7 @@ const defaultState = {
     {
       catName: "Fruits",
       catImgUrl:
-        "https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-2foodgroups_vegetables_detailfeature.jpg?sfvrsn=226f1bc7_6",
+        "https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-3foodgroups_fruits_detailfeature.jpg?sfvrsn=64942d53_4",
       inStock: 100,
       totalSold: 50,
       isActive: true,
@@ -19,7 +19,7 @@ const defaultState = {
     {
       catName: "Electronics",
       catImgUrl:
-        "https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-2foodgroups_vegetables_detailfeature.jpg?sfvrsn=226f1bc7_6",
+        "https://www.ceiworldexpo.com/img/india-market-info/electronics.jpeg",
       inStock: 100,
       totalSold: 50,
       isActive: true,
@@ -33,7 +33,7 @@ const defaultState = {
       price: "150",
       availableStock: "50",
       prodImgUrl:
-        "https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Tomato_je.jpg/1200px-Tomato_je.jpg",
       category: "Vegetables",
       totalSold: 5,
     },
@@ -44,8 +44,8 @@ const defaultState = {
       price: "350",
       availableStock: "50",
       prodImgUrl:
-        "https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg",
-      category: "Electronics",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_OPWQChUB0oD4_jhDQLgxXjJqZVnLTmzALw&s",
+      category: "Fruits",
       totalSold: 5,
     },
     {
@@ -55,8 +55,41 @@ const defaultState = {
       price: "100",
       availableStock: "50",
       prodImgUrl:
-        "https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg",
-      category: "Fruits",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Cptvdisplay.jpg/1200px-Cptvdisplay.jpg",
+      category: "Electronics",
+      totalSold: 5,
+    },
+    {
+      prodName: "Ladys finger",
+      prodDesc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+      price: "50",
+      availableStock: "50",
+      prodImgUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiG3TaaVlr-q0cJ8gsWYiKqWLJPphG3radjw&s",
+      category: "Vegetables",
+      totalSold: 5,
+    },
+    {
+      prodName: "Potato",
+      prodDesc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+      price: "250",
+      availableStock: "50",
+      prodImgUrl:
+        "https://cdn.mos.cms.futurecdn.net/iC7HBvohbJqExqvbKcV3pP-1200-80.jpg",
+      category: "Vegetables",
+      totalSold: 5,
+    },
+    {
+      prodName: "Spinach",
+      prodDesc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+      price: "80",
+      availableStock: "50",
+      prodImgUrl:
+        "https://www.rhs.org.uk/getmedia/743086ee-1ff9-448f-bf4c-69f911765cfa/spinach-Emilia3x2.jpg",
+      category: "Vegetables",
       totalSold: 5,
     },
   ],
