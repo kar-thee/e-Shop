a3fbad11a4148e76770fa06eac3e75d209f79f68
Kartheekeyan S <fsdkartheek@gmail.com>
2024-10-03 11:40:38 +0530

    Category and product management pages added

--- src/pages/settings/CategoryMgmt.jsx
+++ src/pages/settings/CategoryMgmt.jsx
@@ -1,7 +1,57 @@
+import {
+  Button,
+  CardActions,
+  Container,
+  Grid2,
+  TextField,
+  Typography,
+} from "@mui/material";
+
 function CategoryMgmt() {
   return (
     <>
-      <div>Hello CategoryMgmt</div>
+      <Container maxWidth="lg" sx={{ my: 5, justifyContent: "center" }}>
+        <Typography variant="h6" sx={{ my: 2, pb: 2 }}>
+          Add Category
+        </Typography>
+
+        <Grid2
+          container
+          sx={{ height: "250px", pl: 2 }}
+          direction={"column"}
+          justifyContent={"space-evenly"}
+        >
+          <Grid2 size={{ xs: 12, md: 6 }}>
+            <TextField
+              label="Category Name"
+              variant="outlined"
+              sx={{ width: "100%" }}
+            />
+          </Grid2>
+
+          <Grid2 size={{ xs: 12, md: 6 }}>
+            <TextField
+              label="Category Image URL"
+              variant="outlined"
+              sx={{ width: "100%" }}
+            />
+          </Grid2>
+        </Grid2>
+
+        <CardActions sx={{ display: "flex", mb: 1, pl: 2.5 }}>
+          <Button
+            size="large"
+            variant="contained"
+            sx={{
+              backgroundColor: "#6067b3",
+              textTransform: "none",
+              mt: 1,
+            }}
+          >
+            Create
+          </Button>
+        </CardActions>
+      </Container>
     </>
   );
 }
--- src/pages/settings/ProductMgmt.jsx
+++ src/pages/settings/ProductMgmt.jsx
@@ -1,7 +1,82 @@
+import {
+  Button,
+  CardActions,
+  Container,
+  Grid2,
+  TextField,
+  Typography,
+} from "@mui/material";
+
 function ProductMgmt() {
   return (
     <>
-      <div>Hello ProductMgmt</div>
+      <Container maxWidth="lg" sx={{ my: 5, justifyContent: "center" }}>
+        <Typography variant="h6" sx={{ my: 2, pb: 2 }}>
+          Add Product
+        </Typography>
+
+        <Grid2
+          container
+          sx={{ height: "550px", pl: 2 }}
+          direction={"column"}
+          justifyContent={"space-evenly"}
+        >
+          <Grid2 size={{ xs: 12, md: 6 }}>
+            <TextField
+              label="Product Name"
+              variant="outlined"
+              sx={{ width: "100%" }}
+            />
+          </Grid2>
+
+          <Grid2 size={{ xs: 12, md: 6 }}>
+            <TextField
+              label="Description"
+              variant="outlined"
+              sx={{ width: "100%" }}
+            />
+          </Grid2>
+
+          <Grid2 size={{ xs: 12, md: 6 }} direction={"row"}>
+            <TextField
+              label="Price"
+              variant="outlined"
+              sx={{ width: "45%", mr: 2 }}
+            />
+            <TextField label="Stock" variant="outlined" sx={{ width: "45%" }} />
+          </Grid2>
+
+          {/* <Grid2 size={{ xs: 6, md: 5 }}>
+            <TextField
+              label="Stock"
+              variant="outlined"
+              sx={{ width: "100%" }}
+            />
+          </Grid2> */}
+
+          <Grid2 size={{ xs: 12, md: 6 }}>
+            <TextField
+              label="Product Image Url"
+              variant="outlined"
+              sx={{ width: "100%" }}
+            />
+          </Grid2>
+        </Grid2>
+
+        <CardActions sx={{ display: "flex", mb: 1, pl: 2.5 }}>
+          <Button
+            size="large"
+            variant="contained"
+            sx={{
+              backgroundColor: "#6067b3",
+              textTransform: "none",
+              mt: 1,
+            }}
+          >
+            Create
+          </Button>
+        </CardActions>
+      </Container>
     </>
   );
 }
